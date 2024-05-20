8564346d07218ce367db8118492b458f61202e22
kxtriendev04 <khuctrienkxt@gmail.com>
2024-05-20 23:48:33 +0700

    upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const menuBtn = document.getElementById('menu-btn')
+const navbar = document.getElementById('navbar')
+const sections = document.querySelectorAll('section')
+const navLinks = document.querySelectorAll('header nav a')
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                console.log(link)
+                link.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
